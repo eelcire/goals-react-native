7e094031c4f606772d43bd0d234f4c382bec1689
eelcire <eelcire.eelcire@hotmail.com>
2020-02-04 16:48:42 -0600

    second draft

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, FlatList } from 'react-native';
+
+import GoalItem from './components/GoalItem'
+import GoalInput from './components/GoalInput'
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([])
+  const [isAddMode, setIsAddMode] = useState(false)
+
+  const addGoalHander = goalTitle => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: goalTitle }
+    ])
+    setIsAddMode(false)
+    //this method is better cause it's guaranteed to always bring in the most current snapshot of the state
+  }
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter(goal => goal.id !== goalId)
+    })
+  }
+  
+  const cancelGoalAdditionHandler = () => {
+    setIsAddMode(false)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <Button title = "Add New Goal" onPress = {() => setIsAddMode(true)} />
+      <GoalInput visible = {isAddMode} onAddGoal = {addGoalHander} onCancel = {cancelGoalAdditionHandler} />
+      <FlatList
+        keyExtractor = {(item, index) => item.id}
+        data = {courseGoals}
+        renderItem = {item =>
+          <GoalItem title = {item.item.value} onDelete = {removeGoalHandler} id = {item.item.id} />
+        }
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  screen: {
+    padding: 50
+  }
 });
